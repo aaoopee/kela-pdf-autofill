089ed7e241e06c808e6b8481578d1a2c27319c13
aaoopee <antti.pelkonen@iki.fi>
2017-01-24 19:35:28 +0200

    Initial commit of basic files

---
+++ server.js
@@ -0,0 +1,19 @@
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var _ = require('underscore');
+var bcrypt = require('bcrypt');
+
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+app.get('/', function(req, resp) {
+	resp.send('kela-pdf-autofill');
+});
+
+app.listen(PORT, function() {
+	console.log('Server started on port '+PORT+'.');
+});
