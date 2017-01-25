2648db15ba521c0983793390b3cf69869d225f1a
aaoopee <antti.pelkonen@iki.fi>
2017-01-25 21:10:46 +0200

    First try on Kela pdf

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
 var bcrypt = require('bcrypt');
-
+var pdfFiller = require('pdffiller');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -11,7 +11,19 @@ var PORT = process.env.PORT || 3000;
 app.use(bodyParser.json());
 
 app.get('/', function(req, resp) {
-	resp.send('kela-pdf-autofill');
+
+	var sourcePDF = "http://www.kela.fi/documents/10192/3423776/TO1_W.pdf/472f05d6-e804-491b-b576-18a45c57f5ae"
+	var nameRegex = null;
+
+	var FDF_data = pdfFiller.generateFDFTemplate(sourcePDF, nameRegex,
+		function(err, fdfData) {
+			if (err) {
+				console.error(err);
+			}
+			console.log(fdfData);
+		})
+
+	resp.send(FDF_data.toJSON());
 });
 
 app.listen(PORT, function() {
