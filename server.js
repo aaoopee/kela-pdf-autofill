9799bb54338511dfcfe64b0977435e3bf0829b9f
aaoopee <antti.pelkonen@iki.fi>
2017-01-25 21:37:38 +0200

    Some more tweaking

--- server.js
+++ server.js
@@ -11,19 +11,7 @@ var PORT = process.env.PORT || 3000;
 app.use(bodyParser.json());
 
 app.get('/', function(req, resp) {
-
-	var sourcePDF = "http://www.kela.fi/documents/10192/3423776/TO1_W.pdf/472f05d6-e804-491b-b576-18a45c57f5ae"
-	var nameRegex = null;
-
-	var FDF_data = pdfFiller.generateFDFTemplate(sourcePDF, nameRegex,
-		function(err, fdfData) {
-			if (err) {
-				console.error(err);
-			}
-			console.log(fdfData);
-		})
-
-	resp.send(FDF_data.toJSON());
+	resp.send('done');
 });
 
 app.listen(PORT, function() {
