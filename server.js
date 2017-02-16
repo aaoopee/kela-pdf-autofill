6e34883734086031ccdd13ee72fffb3123aebb55
aaoopee <antti.pelkonen@iki.fi>
2017-02-16 15:37:57 +0200

    /doc path for testing document generation in Heroku.

--- server.js
+++ server.js
@@ -8,11 +8,15 @@ var pdfFiller = require('pdffiller');
 var app = express();
 var PORT = process.env.PORT || 3000;
 
+
+var toFill = {
+	"Given Name Text Box": "Antti"
+}
+
+
 app.use(bodyParser.json());
 
-app.get('/', function(req, resp) {
-	console.log(pdfFiller);
-
+app.get('/fdf', function(req, resp) {
 	var sourcePDF = 'http://foersom.com/net/HowTo/data/OoPdfFormExample.pdf';
 	var FDF_data = pdfFiller.generateFDFTemplate(sourcePDF, null, function(err,fdfData) {
 		console.log('Generating FDF data');
@@ -26,6 +30,23 @@ app.get('/', function(req, resp) {
 		}
 	});
 });
+
+app.get('/doc', function(req, resp) {
+	console.log('Generating doc.')
+
+	var sourcePDF = 'http://foersom.com/net/HowTo/data/OoPdfFormExample.pdf';
+
+	pdfFiller.fillFormWithFlatten(sourcePDF, "./tst.pdf", toFill, false, function(err) {
+		if (err) {
+			console.error('Error '+err);
+		}
+
+		resp.status(404).send(err);
+	});
+
+	resp.send('End of doc generation function. Callback may still be active.');
+});
+
 
 app.listen(PORT, function() {
 	console.log('Server started on port '+PORT+'.');
